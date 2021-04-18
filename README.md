43eb77fe61615468b09cbd0c0319d7df0e5011c5
fabian <fabian.fernandeza@sansano.usm.cl>
2021-04-17 20:12:30 -0400

    logs listos

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Intento de Readme
+Instrucciones:
+
+instalacion: solo utiliza la librería time que viene por defecto instalada con python3
+
+instrucciones de Uso:
+- Ejecutar en la terminal "python3 Tarea1.py" o utilizar algun IDE para ejecutar el .py
+- Ingresar cadena a analizar
+- Observar resultados en la terminal
---
+++ Tarea1.py
@@ -0,0 +1,31 @@
+import time
+
+def log(mensaje):
+    FechayHora = time.strftime("%d-%m-%Y %H:%M:%S")
+    print(FechayHora +" "+ mensaje)
+    return
+
+def main():
+    print("Hola, ingresa la cadena de caracteres: ", end ="")
+    cadena = input()
+    log("Entrada: "+cadena)
+    numeroDeCaracteresRepetidos = 0
+    cadenaComprimida = ""
+    caracterAnalizado = cadena[0]
+    for index in range(len(cadena)):
+        if caracterAnalizado == cadena[index]:
+            numeroDeCaracteresRepetidos +=1
+        else:
+            cadenaComprimida+= caracterAnalizado + str(numeroDeCaracteresRepetidos)
+            numeroDeCaracteresRepetidos = 1
+            caracterAnalizado = cadena[index]
+    cadenaComprimida+= caracterAnalizado + str(numeroDeCaracteresRepetidos)
+    output =""
+    if len(cadenaComprimida) > len(cadena):
+        output = cadena
+    else:
+        output = cadenaComprimida
+    log("Salida: "+output)
+    return 0
+
+main()
